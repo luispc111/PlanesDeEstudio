b44b8d69be12d3ce22919ca0baa8e165061eb924
Luis Alberto Pérez Chapa <31547300+luispc111@users.noreply.github.com>
2019-06-08 14:23:20 -0500

    "onclick" for semester created, bugs to be checked

--- methods.js
+++ methods.js
@@ -1,6 +1,7 @@
 //test alerts
 alert("te an jakeado");
 
+// checking classes
 function checking(id) {
 
 	var aux0 = "b" + id;
@@ -56,134 +57,44 @@ function checking(id) {
 	}	
 }
 
-// SEMESTRE BUTTONS
+// checking semesters
+function checkingSemester(id) {
+	
+	// id 	== 	sem1
+	// auxB == 	bsem1
+	// id[3] == 1
 
-// Semestre 1
-document.getElementById("bsem1").onclick = function(){
-	if(document.getElementById("bsem1").checked == true){
+	var auxB = "b" + id;
+	if(document.getElementById(auxB).checked == true){
+		document.getElementById(id).style.color = "white";
+		document.getElementById(id).style.backgroundColor = "green";
+		
+		for(var i = 1; i <= 6; i++){
+			document.getElementById("s" + id[3] "m" + i).style.color = "white";
+			document.getElementById("s" + id[3] "m" + i).style.backgroundColor = "green";
+			document.getElementById("bs" + id[3] "m" + i).checked = true;
+		}
 
-		document.getElementById("sem1").style.color = "white";
-		document.getElementById("sem1").style.backgroundColor = "green";
-
-		document.getElementById("s1m1").style.color = "white";
-		document.getElementById("s1m1").style.backgroundColor = "green";
-		document.getElementById("bs1m1").checked = true;
-
-		document.getElementById("s1m2").style.color = "white";
-		document.getElementById("s1m2").style.backgroundColor = "green";
-		document.getElementById("bs1m2").checked = true;
-
-		document.getElementById("s1m3").style.color = "white";
-		document.getElementById("s1m3").style.backgroundColor = "green";
-		document.getElementById("bs1m3").checked = true;
-
-		document.getElementById("s1m4").style.color = "white";
-		document.getElementById("s1m4").style.backgroundColor = "green";
-		document.getElementById("bs1m4").checked = true;
-
-		document.getElementById("s1m5").style.color = "white";
-		document.getElementById("s1m5").style.backgroundColor = "green";
-		document.getElementById("bs1m5").checked = true;
-
-		document.getElementById("s1m6").style.color = "white";
-		document.getElementById("s1m6").style.backgroundColor = "green";
-		document.getElementById("bs1m6").checked = true;
-
-		document.getElementById("s1m7").style.color = "white";
-		document.getElementById("s1m7").style.backgroundColor = "green";
-		document.getElementById("bs1m7").checked = true;
+		if(id[3] == '1' || id[3] == '8' || id[3] == '9'){
+			document.getElementById("s" + id[3] + "m7").style.color = "white";
+			document.getElementById("s" + id[3] + "m7").style.backgroundColor = "green";
+			document.getElementById("bs" + id[3] + "m7").checked = true;
+		}
 	}
 	else{
-		document.getElementById("sem1").style.color = "black";
-		document.getElementById("sem1").style.backgroundColor = "orange";
+		document.getElementById(id).style.color = "black";
+		document.getElementById(id).style.backgroundColor = "orange";
+		
+		for(var i = 1; i <= 6; i++){
+			document.getElementById("s" + id[3] "m" + i).style.color = "black";
+			document.getElementById("s" + id[3] "m" + i).style.backgroundColor = "orange";
+			document.getElementById("bs" + id[3] "m" + i).checked = false;
+		}
 
-		document.getElementById("s1m1").style.color = "black";
-		document.getElementById("s1m1").style.backgroundColor = "orange";
-		document.getElementById("bs1m1").checked = false;
-
-		document.getElementById("s1m2").style.color = "black";
-		document.getElementById("s1m2").style.backgroundColor = "orange";
-		document.getElementById("bs1m2").checked = false;	
-
-		document.getElementById("s1m3").style.color = "black";
-		document.getElementById("s1m3").style.backgroundColor = "orange";
-		document.getElementById("bs1m3").checked = false;
-
-		document.getElementById("s1m4").style.color = "black";
-		document.getElementById("s1m4").style.backgroundColor = "orange";
-		document.getElementById("bs1m4").checked = false;
-
-		document.getElementById("s1m5").style.color = "black";
-		document.getElementById("s1m5").style.backgroundColor = "orange";
-		document.getElementById("bs1m5").checked = false;
-
-		document.getElementById("s1m6").style.color = "black";
-		document.getElementById("s1m6").style.backgroundColor = "orange";
-		document.getElementById("bs1m6").checked = false;
-
-		document.getElementById("s1m7").style.color = "black";
-		document.getElementById("s1m7").style.backgroundColor = "orange";
-		document.getElementById("bs1m7").checked = false;
-	}
-}
-
-// Semestre 2
-document.getElementById("bsem2").onclick = function(){
-	if(document.getElementById("bsem2").checked == true){
-
-		document.getElementById("sem2").style.color = "white";
-		document.getElementById("sem2").style.backgroundColor = "green";
-
-		document.getElementById("s2m1").style.color = "white";
-		document.getElementById("s2m1").style.backgroundColor = "green";
-		document.getElementById("bs2m1").checked = true;
-
-		document.getElementById("s2m2").style.color = "white";
-		document.getElementById("s2m2").style.backgroundColor = "green";
-		document.getElementById("bs2m2").checked = true;
-
-		document.getElementById("s2m3").style.color = "white";
-		document.getElementById("s2m3").style.backgroundColor = "green";
-		document.getElementById("bs2m3").checked = true;
-
-		document.getElementById("s2m4").style.color = "white";
-		document.getElementById("s2m4").style.backgroundColor = "green";
-		document.getElementById("bs2m4").checked = true;
-
-		document.getElementById("s2m5").style.color = "white";
-		document.getElementById("s2m5").style.backgroundColor = "green";
-		document.getElementById("bs2m5").checked = true;
-
-		document.getElementById("s2m6").style.color = "white";
-		document.getElementById("s2m6").style.backgroundColor = "green";
-		document.getElementById("bs2m6").checked = true;
-	}
-	else{
-		document.getElementById("sem2").style.color = "black";
-		document.getElementById("sem2").style.backgroundColor = "orange";
-
-		document.getElementById("s2m1").style.color = "black";
-		document.getElementById("s2m1").style.backgroundColor = "orange";
-		document.getElementById("bs2m1").checked = false;
-
-		document.getElementById("s2m2").style.color = "black";
-		document.getElementById("s2m2").style.backgroundColor = "orange";
-		document.getElementById("bs2m2").checked = false;	
-
-		document.getElementById("s2m3").style.color = "black";
-		document.getElementById("s2m3").style.backgroundColor = "orange";
-		document.getElementById("bs2m3").checked = false;
-
-		document.getElementById("s2m4").style.color = "black";
-		document.getElementById("s2m4").style.backgroundColor = "orange";
-		document.getElementById("bs2m4").checked = false;
-
-		document.getElementById("s2m5").style.color = "black";
-		document.getElementById("s2m5").style.backgroundColor = "orange";
-		document.getElementById("bs2m5").checked = false;
-
-		document.getElementById("s2m6").style.color = "black";
-		document.getElementById("s2m6").style.backgroundColor = "orange";
-		document.getElementById("bs2m6").checked = false;
+		if(id[3] == '1' || id[3] == '8' || id[3] == '9'){
+			document.getElementById("s" + id[3] + "m7").style.color = "black";
+			document.getElementById("s" + id[3] + "m7").style.backgroundColor = "orange";
+			document.getElementById("bs" + id[3] + "m7").checked = false;
+		}
 	}
 }
