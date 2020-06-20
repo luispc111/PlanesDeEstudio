042ab0a6101a9872bfd01469da9847116a7a2974
Luis Alberto Pérez Chapa <luispc111@yahoo.com>
2020-06-20 13:41:53 -0500

    fixed background from download image

--- src/downloadPlan.js
+++ src/downloadPlan.js
@@ -3,9 +3,18 @@
 const $boton = document.querySelector("#btnCapturar"), // El botón que desencadena
       $objetivo = document.querySelector("#planDeEstudios"); // A qué le tomamos la foto
 
-
 $boton.addEventListener("click", () => {
-    html2canvas($objetivo)
+    const options = {
+        // ignoreElements: elemento => {
+        //   const tipo = elemento.nodeName.toLowerCase();
+        //   if (tipo === "img" || tipo === "h1") {
+        //     return true;
+        //   }
+        //   return false;
+        // }
+        backgroundColor: "#212529"
+      };
+    html2canvas($objetivo, options)
       .then(canvas => {
         let enlace = document.createElement('a');
         enlace.download = "MiPlanDeEstudios.png";
