ebe63c842f1a3b621e93a5ea3ce5b356bd3fa7ca
alegayndra <alegayndra@gmail.com>
2021-03-28 17:46:29 -0600

    Agregué archivos

---
+++ src/components/PlanesDeEstudio/BotonCarrera.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BotonCarrera() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/PlanesDeEstudio/PlanesDeEstudio.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PlanesDeEstudio() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
