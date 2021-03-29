549bc2abb604ddb4f9e5c3f5d744d6c8704fee13
alegayndra <alegayndra@gmail.com>
2021-03-28 18:48:33 -0600

    Cambié nombre de componentes del boton

--- src/components/PlanesDeEstudio/BotonCarrera.js
+++ src/components/PlanesDeEstudio/BotonCarrera.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export default function BotonCarrera(props) {
-    const { boton } = props;
-    const { nombre, id } = boton;
+    const { carrera } = props;
+    const { nombre, clave } = carrera;
 
     return (
         <div>
             <button
                 type="button"
                 className="btn btn-primary btn-lg selectStudyPlan"
-                value={id}
+                value={clave}
             >
                 {nombre}
             </button>
