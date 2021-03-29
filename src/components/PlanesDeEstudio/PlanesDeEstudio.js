ff4fc49d7a6d47d11b3a6d1942004e0b354b52b3
alegayndra <alegayndra@gmail.com>
2021-03-28 18:39:09 -0600

    Corregí nombres de atributos de jsx

--- src/components/PlanesDeEstudio/PlanesDeEstudio.js
+++ src/components/PlanesDeEstudio/PlanesDeEstudio.js
@@ -4,18 +4,29 @@ import BotonCarrera from './BotonCarrera';
 export default function PlanesDeEstudio() {
     return (
         <div>
-            <h1 class="titulo">Planes de estudio</h1>
-            <div class="form-group">
-				<h2 for="studyPlanSelected" color="white;">Selecciona tu plan de estudios:</h2>
-				<form onSubmit="return false;">
-					<div class="input-group">
-					  	<input type="text" class="form-control" id="searchStudyPlan" placeholder="Search" autocomplete="off" />
-					  	<div class="input-group-btn">
-						</div> 
+            <h1 className="titulo">Planes de estudio</h1>
+            <div className="form-group">
+				<form onSubmit={() => false}>
+					<div className="input-group">
+				        <h2
+                            htmlFor="studyPlanSelected"
+                            color="white;"
+                        >
+                            Selecciona tu plan de estudios:
+                        </h2>
+					  	<input
+                          type="text"
+                          className="form-control"
+                          id="searchStudyPlan"
+                          placeholder="Search"
+                          autoComplete="off"
+                        />
 					</div>
 				</form>
 				<div id="majorBtns">
-                    <BotonCarrera />
+                    <BotonCarrera
+                        boton={{nombre: 'a', id: 'a'}}
+                    />
 				</div>
 			</div>
         </div>
