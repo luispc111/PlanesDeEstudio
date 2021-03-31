da6fb5525ef25117d255d879b287a6992e3116d3
alegayndra <alegayndra@gmail.com>
2021-03-31 17:54:15 -0600

    Cambié cómo se renderean los botones

--- src/components/PlanesDeEstudio/BotonCarrera.js
+++ src/components/PlanesDeEstudio/BotonCarrera.js
@@ -1,18 +1,16 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
 export default function BotonCarrera(props) {
     const { carrera } = props;
     const { nombre, clave } = carrera;
 
     return (
-        <div>
-            <button
-                type="button"
-                className="bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 rounded text-sm md:text-base selectStudyPlan"
-                value={clave}
-            >
-                {nombre}
-            </button>
-        </div>
+        <Button 
+            variant="primary"
+            value={clave}
+        >
+            {nombre}    
+        </Button>
     )
 }
--- src/components/PlanesDeEstudio/PlanesDeEstudio.js
+++ src/components/PlanesDeEstudio/PlanesDeEstudio.js
@@ -1,76 +1,72 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { Button } from 'react-bootstrap';
+
 import BotonCarrera from './BotonCarrera';
 
+
 export default function PlanesDeEstudio() {
-    const [planesDeEstudio, setPlanesDeEstudio] = useState([]);
+  const [planesDeEstudio, setPlanesDeEstudio] = useState([]);
 
-    useEffect(() => {
-        let planes = [
-            {nombre: 'ITC 11', clave: 'itc_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-            {nombre: 'LAD 11', clave: 'lad_11',},
-        ];
+  useEffect(() => {
+    let planes = [
+      {nombre: 'ITC 11', clave: 'itc_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+      {nombre: 'LAD 11', clave: 'lad_11',},
+    ];
 
-        setPlanesDeEstudio(planes);
-    }, []);
+    setPlanesDeEstudio(planes);
+  }, []);
 
-    return (
+  return (
+    <div>
+      <h1 className="titulo">Planes de estudio</h1>
+      <div className="form-group">
+        <form onSubmit={() => false}>
+          <div className="input-group">
+            <h2
+              htmlFor="studyPlanSelected"
+              className="block text-sm font-bold mb-2 text-white"
+            >
+                Selecciona tu plan de estudios:
+            </h2>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline form-control"
+              id="searchStudyPlan"
+              placeholder="Search"
+              autoComplete="off"
+            />
+          </div>
+        </form>
         <div>
-            <h1 className="titulo">Planes de estudio</h1>
-            <div className="form-group">
-				<form onSubmit={() => false}>
-					<div className="input-group">
-				        <h2
-                            htmlFor="studyPlanSelected"
-                            className="block text-sm font-bold mb-2 text-white"
-                        >
-                            Selecciona tu plan de estudios:
-                        </h2>
-					  	<input
-                          type="text"
-                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline form-control"
-                          id="searchStudyPlan"
-                          placeholder="Search"
-                          autoComplete="off"
-                        />
-					</div>
-				</form>
-				<BotonesCarreras
-                    listaCarreras={planesDeEstudio}
-                />
-			</div>
+          {
+            planesDeEstudio.map(({clave, nombre}, indice) => (
+              <Button 
+                key={indice}
+                variant="primary"
+                value={clave}
+              >
+                {nombre}    
+              </Button>
+            ))
+          }
         </div>
-    )
-}
-
-function BotonesCarreras(props) {
-    const { listaCarreras } = props;
-
-    return (
-        <div className="grid grid-cols-12 auto-rows-min gap-y-5" id="majorBtns">
-            {
-                listaCarreras.map((carrera, indice) => (
-                    <BotonCarrera
-                        key={indice}
-                        carrera={carrera}
-                    />
-                ))
-            }
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
