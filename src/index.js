b17abc043b834a2516f2de75a5d233d198a70ef0
alegayndra <alegayndra@gmail.com>
2021-03-31 17:45:57 -0600

    Cambié Tailwind por Boostrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/index.scss';
-import './sass/tailwind_imports.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
