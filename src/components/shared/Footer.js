f34811943c85cfdfbf0be12c66635a87a6daa07f
alegayndra <alegayndra@gmail.com>
2021-04-04 15:34:57 -0500

    Footer Terminado

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { Col, Container, Navbar, Row, Image } from 'react-bootstrap';
 
 export default function Footer() {
     return (
-        <section style={{color: "white"}} id="credits">
-			<div align="center">
-                Creada por: <a href="https://github.com/luispc111" target="_blank">Luis Alberto Pérez</a> & <a href="https://github.com/alegayndra" target="_blank">Alejandra García</a>
-            </div>
-			<a href="https://github.com/luispc111/PlanesDeEstudio" target="_blank">
-				<img src="GitHub-Mark-Light-32px.png" alt="Github link" className="centerImage" />
-			</a>
-		</section>
+        <Navbar fixed="bottom">
+            <Container style={{color: "white"}} id="credits">
+                <Col>
+                    <Row>
+                        <Col>
+                            <p>
+                                Creada por:
+                                <a href="https://github.com/Adrian-Garcia" target="_blank"> Adrián García</a>,
+                                <a href="https://github.com/alegayndra" target="_blank"> Alejandra García</a>,
+                                <a href="https://github.com/EstradaDiego99" target="_blank"> Diego Estrada</a>,
+                                &
+                                <a href="https://github.com/luispc111" target="_blank"> Luis Alberto Pérez </a>
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row className="p-2">
+                        <Col>
+                            <a href="https://github.com/luispc111/PlanesDeEstudio" target="_blank">
+                                <Image src="./GitHub-Mark-Light-32px.png" alt="Github link"/>
+                            </a>
+                        </Col>
+                    </Row>
+                </Col>
+            </Container>
+        </Navbar>
     )
 }
