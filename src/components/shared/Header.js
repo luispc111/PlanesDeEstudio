eaed4bc218adf4dbcfc1436fe358f43c339b0472
Luis Alberto Pérez Chapa <luispc111@yahoo.com>
2021-04-04 18:51:26 -0500

    created header title and options

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>
-            
+        <div class="App-header">
+            <div class="header-left">
+                <a href="" class="element">Planes de Estudio</a>
+            </div>
+            <div class="header-right">
+                <a href="" class="element">Perfil</a>
+                <a href="" class="element">Cerrar sesión</a>
+            </div>
         </div>
     )
 }
