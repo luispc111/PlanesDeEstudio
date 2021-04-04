8c6d58a9e136fd73e54cb72d54dd44a145bb7f62
alegayndra <alegayndra@gmail.com>
2021-04-04 15:15:30 -0500

    Agregué footer original

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            
-        </div>
+        <section style={{color: "white"}} id="credits">
+			<div align="center">
+                Creada por: <a href="https://github.com/luispc111" target="_blank">Luis Alberto Pérez</a> & <a href="https://github.com/alegayndra" target="_blank">Alejandra García</a>
+            </div>
+			<a href="https://github.com/luispc111/PlanesDeEstudio" target="_blank">
+				<img src="GitHub-Mark-Light-32px.png" alt="Github link" className="centerImage" />
+			</a>
+		</section>
     )
 }
