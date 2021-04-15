871380a5ae42359050dbd682057568cd024d0a6d
alegayndra <alegayndra@gmail.com>
2021-04-15 09:58:51 -0500

    Arreglé ruta de la lista de los planes de estudio

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Route
-          path="/"
+          exact path="/"
           component={PlanesDeEstudio}
         />
       </Router>
