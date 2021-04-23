8315d79fd361016c611e5c408f51ae84342dfab6
alegayndra <alegayndra@gmail.com>
2021-04-22 20:40:27 -0500

    Redondeo de porcentajes de barras de progreso

--- src/components/PlanDeEstudio/BarrasDeProgreso/BarrasDeProgreso.js
+++ src/components/PlanDeEstudio/BarrasDeProgreso/BarrasDeProgreso.js
@@ -5,7 +5,7 @@ export default function BarrasDeProgreso({ listaColores, cantMateriasPorColor, t
   return (
     <ProgressBar className="m-0 barra-progreso">
       {listaColores.map((color, indice) => {
-        let cant = (cantMateriasPorColor[color] / totalMaterias * 100) || 0;
+        let cant = (cantMateriasPorColor[color] / totalMaterias * 100).toFixed(2) || 0;
         return (
           <ProgressBar
             // striped
