ab52731a80b56a65dd8abb951ae1192d1c3b45d1
alegayndra <alegayndra@gmail.com>
2021-04-25 11:11:30 -0500

    Utilicé componentes de bootstrap

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Navbar, Image, Nav } from 'react-bootstrap'
 
 /**
  * Parte superior, contiene controles de manejo de sesión y perfil
@@ -6,15 +7,26 @@ import React from 'react'
  * @param {Boolean} sesionIniciada Indica si el usuario ingresa como previamente registrado.
  */
 export default function Header({ sesionIniciada }) {
-    return (
-        <div class="header">
-            <div class="header-left">
-                <a href="" class="element">Planes de Estudio</a>
-            </div>
-            <div class="header-right">
-                {sesionIniciada && (<a href="" class="element">Ver perfil <img src="https://i.stack.imgur.com/l60Hf.png" class="rounded-circle profile-image"></img></a>)}
-                {!sesionIniciada && (<a href="" class="element">Iniciar sesión</a>)}
-            </div>
-        </div>
-    )
+  return (
+    <Navbar sticky="top" variant="dark" className="p-0 pb-4">
+      <h1> Planes de Estudio </h1>
+      <Navbar.Collapse className="justify-content-end">
+        <Nav className="justify-content-end element">
+          {sesionIniciada && (
+            <Nav.Link href="/">
+              Ver perfil
+              <Image
+                className="ml-4" 
+                width={48}
+                height={48}
+                src="https://i.stack.imgur.com/dr5qp.jpg"
+                roundedCircle
+              />
+            </Nav.Link>)
+          }
+          {!sesionIniciada && (<Nav.Link href="/"> Iniciar sesión </Nav.Link>)}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
