de3db274251ef530bed5c3d579c9b600a18c4343
alegayndra <alegayndra@gmail.com>
2021-04-25 21:38:55 -0500

    Corregí estilo de periodos

--- src/components/PlanDeEstudio/Semestre/Materia/Materia.js
+++ src/components/PlanDeEstudio/Semestre/Materia/Materia.js
@@ -6,19 +6,21 @@ export default function Materia ({ nums, materia, tec21, clickMateria }) {
   const {numSemestre, numMateria} = nums
 
   return (
-    <div className={`materia labelMateria bg-${materia.color}`} onClick={() => clickMateria(numSemestre, numMateria)}>
-      <Row >
-        <Col>
-          <label>{materia.nombre}</label>
-        </Col>
-      </Row>
-      {tec21 && (
-        <Row className="tec21 p-0 m-0">
-          <Col className={`bloque-tec21-${(materia?.periodos[0]) ? '' : 'no-'}coloreado p-0 m-0`}></Col>
-          <Col className={`bloque-tec21-${(materia?.periodos[1]) ? '' : 'no-'}coloreado p-0 m-0`}></Col>
-          <Col className={`bloque-tec21-${(materia?.periodos[2]) ? '' : 'no-'}coloreado p-0 m-0`}></Col>
+    <Row className={`p-0 m-0 materia labelMateria bg-${materia.color}`} onClick={() => clickMateria(numSemestre, numMateria)}>
+      <Col className="p-0 m-0">
+        <Row className="nombre-materia p-0 m-0">
+          <Col className="p-0 m-0">
+            <label>{materia.nombre}</label>
+          </Col>
         </Row>
-      )}
-    </div>
+        {tec21 && (
+          <Row className="tec21 p-0 m-0">
+            <Col className={`bloque-tec21-${(materia?.periodos[0]) ? '' : 'no-'}seleccionado p-0 m-0`}></Col>
+            <Col className={`bloque-tec21-${(materia?.periodos[1]) ? '' : 'no-'}seleccionado p-0 m-0`}></Col>
+            <Col className={`bloque-tec21-${(materia?.periodos[2]) ? '' : 'no-'}seleccionado p-0 m-0`}></Col>
+          </Row>
+        )}
+      </Col>
+    </Row>
   )
 }
