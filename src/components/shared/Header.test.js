c8300bec13628eca3963a3305f87b7a848e0aa63
Diego Estrada Talamantes <devdiegoest@gmail.com>
2021-04-25 18:27:01 -0600

    Actualizar tests para incluír contexto

--- src/components/shared/Header.test.js
+++ src/components/shared/Header.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from '@testing-library/react';
 
 import Header from "./Header";
+import { UserContext } from "../../context";
 
 it("renderiza header cuando no se ha iniciado sesión", () => {
   const { getByText } = render(<Header sesionIniciada={false}/>);
@@ -11,10 +12,13 @@ it("renderiza header cuando no se ha iniciado sesión", () => {
 });
 
 it("renderiza header cuando se ha iniciado sesión", () => {
-  const { getByText, container } = render(<Header sesionIniciada={true}/>);
+  const { getByText, container } = render(
+    <UserContext.Provider value={{urlFoto: "https://i.stack.imgur.com/YaL3s.jpg"}}>
+      <Header />
+    </UserContext.Provider>
+  );
 
   expect(getByText("Planes de Estudio")).toBeInTheDocument();
-  expect(getByText(/Ver perfil/)).toBeInTheDocument();
 
   const imagenPerfil = container.querySelector('.imagen-perfil');
   expect(imagenPerfil).toBeInTheDocument();
