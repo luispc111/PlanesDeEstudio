d5e2240f96ca5d5abef14670db738a161cc8e930
alegayndra <alegayndra@gmail.com>
2021-04-25 19:07:36 -0500

    Actualicé funcionamiento del coloreo de semstre

--- src/components/PlanDeEstudio/Semestre/Semestre.js
+++ src/components/PlanDeEstudio/Semestre/Semestre.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col } from 'react-bootstrap';
 
 import Materia from './Materia/Materia';
@@ -10,9 +10,37 @@ export default function Semestre ({ numSemestre, materias, tec21, colorSeleccion
   const { clickSemestre, clickMateria } = clicks;
 
   const botonClickeado = () => {
-    clickSemestre(numSemestre)
-    setColorDeFondo(colorSeleccionado)
+    clickSemestre(numSemestre);
+    setColorDeFondo(colorSeleccionado);
   }
+
+  // Se actualiza el color del boton del semestre
+  useEffect(() => {
+    // Este if es para colorear el semestre de naranja por si alguna materia se pintó de otro color cuando todas estaban del mismo color
+    if (colorDeFondo !== 'orange') {
+      materias.forEach(materia => {
+        if (materia.color !== colorDeFondo) {
+          setColorDeFondo('orange');
+          return;
+        }
+      });
+    } 
+    // Este else es para colorear el semestre del color de todas las materias cuando todas están del mismo color
+    else {
+      let color = materias[0].color;
+      let cambiarColor = true;
+
+      materias.forEach(materia => {
+        if (materia.color !== color) {
+          cambiarColor = false;
+        }
+      });
+
+      if (cambiarColor) {
+        setColorDeFondo(color);
+      }
+    }
+  }, [materias, colorDeFondo]);
 
   return (
     <Col xs={4} md className="semestre m-0 p-0 mb-4">
